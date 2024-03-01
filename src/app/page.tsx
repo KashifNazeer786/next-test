c311780f2fad8232e67d1bb7acb71a2dde9669e5
Sparko-Sol <accounts@sparkosol.com>
2024-03-01 16:08:02 +0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { HeroScrollDemo } from "../components/ContainerScrollAnimation";
+import { InfiniteMovingCardsDemo } from "../components/InfiniteMovingCard";
+import { ParallaxScrollDemo } from "../components/ParallaxGridScroll";
+import { HeroParallaxDemo } from "../components/HeroParallax";
+import { StickyScrollRevealDemo } from "../components/StickyScrollReveal";
+import { TypewriterEffectSmoothDemo } from "../components/typewriter";
+import { FollowingPointerDemo } from "../components/FollowingCursor";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroScrollDemo />
+      <InfiniteMovingCardsDemo />
+      <ParallaxScrollDemo />
+      <HeroParallaxDemo />
+      <StickyScrollRevealDemo />
+      <TypewriterEffectSmoothDemo />
+      <FollowingPointerDemo />
+    </div>
+  );
+}
